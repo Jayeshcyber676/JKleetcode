2e23b46f5708aeb5cc82f33fc816633b0d5a1bb9
Jayesh Kumar <141473828+Jayeshcyber676@users.noreply.github.com>
2026-09-16 12:31:14 +0530

    Time: 8 ms (21.78%), Space: 132.1 MB (8.6%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -3,13 +3,13 @@ public:
     bool isPalindrome(ListNode* head) {
         vector<int> values;
 
-        // Store linked list values
+         
         while (head != nullptr) {
             values.push_back(head->val);
             head = head->next;
         }
 
-        // Check palindrome
+         
         int left = 0;
         int right = values.size() - 1;
 
